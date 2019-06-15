31b6d980e44fe51a45007ca689af777727e1f44d
cam027 <36745258+cam027@users.noreply.github.com>
2019-06-15 11:42:06 -0700

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+def addup(x, y):
+    sum = x + y
+    print("Sum: {}".format(sum))
+    
+    
+    
+    
+x = int(input("Enter 1st Number: "))
+y = int(input("Enter 2nd number: "))
+
+addup(x, y)
